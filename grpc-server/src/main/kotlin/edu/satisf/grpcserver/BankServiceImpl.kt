58ea7abbedd3b506df0dd1b7372ce98333e7b05a
Philipp Möller <philipp.moeller@sportalliance.com>
2023-04-05 17:03:13 +0200

    implement dummy server response

--- grpc-server/src/main/kotlin/edu/satisf/grpcserver/BankServiceImpl.kt
+++ grpc-server/src/main/kotlin/edu/satisf/grpcserver/BankServiceImpl.kt
@@ -1,5 +1,24 @@
 package edu.satisf.grpcserver
 
+import io.grpc.stub.StreamObserver
+import net.devh.boot.grpc.server.service.GrpcService
 
-class BankServiceImpl {
+
+@GrpcService
+class BankServiceImpl: BankServiceGrpc.BankServiceImplBase() {
+
+    override fun balanceRequest(
+        request: BankAccountBalanceRequest?,
+        responseObserver: StreamObserver<BalanceResponse>
+    ) {
+        val response = BalanceResponse.newBuilder().setCurrentBalance(10.0).build()
+        responseObserver.onNext(response)
+        responseObserver.onCompleted()
+    }
+
+    override fun requestTransfer(request: TransferRequest?, responseObserver: StreamObserver<TransferResponse>) {
+        val response = TransferResponse.newBuilder().setSuccess(true).build()
+        responseObserver.onNext(response)
+        responseObserver.onCompleted()
+    }
 }
